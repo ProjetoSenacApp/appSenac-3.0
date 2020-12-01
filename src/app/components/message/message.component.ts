36dde9ca39521e17f318a0766c6b78a41c5987a0
anderson marley <51322494+jovem-programador@users.noreply.github.com>
2020-12-01 13:49:58 -0300

    mensagem

--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -8,6 +8,10 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { AppService } from '../../services/app.service';
 import { EventsService } from 'src/app/services/events.service';
 
+// 19.1) Importa dependências
+import { DatePipe } from '@angular/common';
+import { AlertController } from '@ionic/angular';
+
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
@@ -24,6 +28,11 @@ export class MessageComponent implements OnInit {
   msgWhon: any; // Dados do correspondente ('from' ou 'to')
   btnText = 'Enviar resposta'; // Texto do botão de envio
 
+  // 19.3) Cria atributos
+  msgReply: any; // Mensagem de resposta
+  msgSend: any; // Mensagem a ser enviada
+  pipe = new DatePipe('en_US'); // Formatação da data
+
   constructor(
 
     // 15.2) Injeta dependências
@@ -34,6 +43,9 @@ export class MessageComponent implements OnInit {
     public app: AppService,
     public events: EventsService,
     private route: ActivatedRoute,
+
+    // 19.2) Injeta dependências
+    public alertController: AlertController,
   ) {
 
     // 15.4) Obtém nome da caixa (inbox / outbox) e Id da mensagem (-> msgParams)
@@ -86,7 +98,9 @@ export class MessageComponent implements OnInit {
       ],
 
       message: [
-        `\n\nMensagem original: \n--------------------\n${this.msgToReply.message}\n--------------------\n`,
+
+        // 19.10) Mensagem da resposta
+        null,
         Validators.compose([
           Validators.required,
           Validators.minLength(5)
@@ -98,7 +112,47 @@ export class MessageComponent implements OnInit {
 
   // 15.10) Processa envio da mensagem
   messageSubmit() {
-    console.log(this.messageForm.value);
+
+    // 19.4) Formata mensagem de resposta
+    const msgOriginal = `\n\n\n--------------------\nMensagem original:\n--------------------\n${this.msgToReply.message}\n--------------------`;
+    this.msgReply = {
+      date: this.pipe.transform(Date.now(), 'yyyy-MM-dd hh:mm:ss').trim(),
+      from: this.userData.uid.trim(),
+      to: this.msgToReply.from.trim(),
+      subject: this.messageForm.value.subject.trim(),
+      message: this.messageForm.value.message.trim() + msgOriginal,
+      status: 'Não lida'
+    };
+
+    // 19.5) Salva mensagem na caixa de entrada do destinatário
+    this.fbStore.collection(`messages/${this.msgReply.to}/inbox`).add(this.msgReply)
+      .then((docRef) => {
+
+        // 19.6) Altera status da mensagem
+        this.msgReply.status = 'Enviada';
+
+        // 19.7) Salva mensagem na caixa de saída do remetente
+        this.fbStore.collection(`messages/${this.msgReply.from}/outbox`).doc(docRef.id).set(this.msgReply)
+          .then(() => {
+
+            // 19.8) Feedback
+            this.presentAlertConfirm();
+          });
+      });
   }
 
+  // 19.9) Feedback
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      header: 'Mensagem enviada',
+      message: 'Sua mensagem foi enviada com sucesso!<br><br>Clique em [Ok] para voltar à caixa de entrada.',
+      buttons: [
+        {
+          text: 'Ok',
+          handler: () => { this.router.navigate(['/msg/inbox']); }
+        }
+      ]
+    });
+    await alert.present();
+  }
 }
--- src/app/msg/outbox/outbox.page.ts
+++ src/app/msg/outbox/outbox.page.ts
@@ -1,9 +1,13 @@
+  
 import { Component, OnInit } from '@angular/core';
 
 // 10.1) Importa dependências
 import { Router } from '@angular/router';
 import { StorageMap } from '@ngx-pwa/local-storage';
 import { AppService } from 'src/app/services/app.service';
+
+// 21.1) Importa dependências
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-outbox',
@@ -16,12 +20,18 @@ export class OutboxPage implements OnInit {
   userData: any;
   userProfile: any;
 
+  // 21.3) Atributos
+  public allMessages: any[any]; // View com todas as mensagens
+
   constructor(
 
     // 10.2) Injeta dependências
     public router: Router,
     public app: AppService,
     public storage: StorageMap,
+
+    // 21.2) Injeta dependências
+    public fbStore: AngularFirestore,
   ) { }
 
   ngOnInit() { }
@@ -46,6 +56,9 @@ export class OutboxPage implements OnInit {
               this.storage.get('userProfile', { type: 'string' }).subscribe(
                 (pData) => {
                   this.userProfile = JSON.parse(pData);
+
+                  // 21.4) Obtém todas as mensagens para a 'view'
+                  this.getAllMessages();
                 }
               );
             }
@@ -55,6 +68,48 @@ export class OutboxPage implements OnInit {
         } else {
           this.router.navigate(['/user/new']);
         }
+      });
+  }
+
+
+  // 21.5) Obtém todas as mensagens para a 'view'
+  getAllMessages() {
+
+    // Lê mesangens do banco de dados com base no Id do usuário logado
+    this.fbStore.collection(
+      `messages/${this.userData.uid}/outbox`,
+      ref => ref.orderBy('date', 'desc')
+    ).valueChanges({ idField: 'msgId' }).subscribe(
+      (mData) => {
+
+        // Variável local para as mensagens
+        const allMessages = [];
+
+        // Obtém cada mensagem recebida
+        mData.forEach(
+          (msgData: any) => {
+
+            // Não exibe mensagens apagadas
+            if (msgData.status !== 'Apagada') {
+
+              // Obtém o nome de que enviou a mensagem
+              this.fbStore.doc<any>(`users/${msgData.from}`).valueChanges().subscribe(
+                (data) => {
+                  msgData.fromName = data.name;
+
+                  // Seleciona ícone conforme status da mensagem
+                  msgData.statusIcon = 'send-outline';
+
+                  // Lista mensagem em "todas as mensagens"
+                  allMessages.push(msgData);
+                }
+              );
+            }
+          }
+        );
+
+        // Envia mensagens para a view
+        this.allMessages = allMessages;
       });
   }
 }
